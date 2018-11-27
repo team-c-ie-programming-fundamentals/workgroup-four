a70a4aa7caa17521e8e83825f6978310e3347553
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-27 11:49:56 +0100

    updated

--- weighted.py
+++ weighted.py
@@ -4,4 +4,58 @@ class WeightedDirected:
     def __init__(self, dictionary): 
         self.dictionary = dictionary 
         
-    def 
+        
+        
+    
+    def find_all_paths(self, start, end, path=[], weight=0):
+        path = path + [{"node": start, "weight": weight}]
+    
+        if start == end:
+            return [path]
+    
+        if not start in self.dictionary:
+            return []
+    
+        paths = []
+        
+        for conn in self.dictionary[start]:
+            if conn not in path:
+                newpaths = self.find_all_paths(conn["node"], end, path, conn["weight"])
+                                
+                for newpath in newpaths:
+                    paths.append(newpath)
+        
+        return paths
+
+                
+    
+#    def cheapest_path(self, start, end):
+#    all_paths = find_all_paths(graph, start, end)
+#    
+#    cheapest = None
+#    
+#    for path in all_paths:
+#        cost = 0
+#        
+#        for step in path:
+#            cost += step["weight"]
+#            
+#        if cheapest is None or cost < cheapest:
+#            cheapest = cost
+#            
+#    return cheapest
+
+
+
+graph2 = {
+    "a": [{"node": "b", "weight": 1}, {"node": "c", "weight": 2}],
+    "b": [{"node": "d", "weight": 3}],
+    "c": [{"node": "d", "weight": 1}],
+    "d": [{"node": "e", "weight": 3}],
+    "e": [{"node": "f", "weight": 3}]
+}
+
+
+graph_ts = WeightedDirected(graph2)
+
+
