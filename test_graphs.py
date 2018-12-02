adba861e57046652f967cda0fa91b62599804be5
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-02 15:12:24 +0100

    there is no path test createdm not working yet

--- test_graphs.py
+++ test_graphs.py
@@ -1,1 +1,18 @@
+#%%
+
 from graphs import find_path
+
+def test_there_is_no_path(): 
+    
+    
+    value =  {
+                "a": ["b","c"],
+                "b": ["c"],
+                "c" : ["b", "d"],
+                "d" : ["b"],
+                }
+
+
+    for case in value:
+        
+        assert find_path(case) == None
