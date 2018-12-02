73bc1881dad12cf436ff5eea73bffa0bd7766442
Lukas Hauser <lukas.raphael.hauser@gmail.com>
2018-12-02 13:48:49 +0100

    Update graphs.py
    Added Alternative to dictionary version with a separate class Nodes.

--- graphs.py
+++ graphs.py
@@ -45,3 +45,66 @@ graph10 =  {
 graph = DirectedGraph(graph10)
 
 graph.find_path("a","b")
+
+#%%
+
+#Alternative Solution
+
+class Node:
+    def __init__(self, label, edges):
+        self.label = label
+        self.edges = edges
+    
+    def new_edge(self, edge):
+        self.edges.append(edge)
+
+a = Node("a", [])
+b = Node("b", [])
+c = Node("c", [])
+d = Node("d", [])
+
+a.new_edge(b)
+a.new_edge(c)
+b.new_edge(c)
+c.new_edge(b)
+c.new_edge(d)
+d.new_edge(b)
+
+class Dir_Graph:
+    def __init__(self, lst):
+        self.lst = lst
+        
+    def find_connection(self, edge):
+        return edge.edges
+    
+    def find_path_alt(self, start, end, path=[]):
+        
+        path = path + [start]
+        
+        if start == end:
+            return path
+    
+        
+        if start not in self.lst:
+            return None
+    
+
+        for node in self.find_connection(start):
+
+            if node not in path:
+
+                newpath = self.find_path_alt(node, end, path)
+
+                if newpath is not None:
+                    return newpath
+      
+        return None
+    
+    def return_connection(self,start,end):
+        lst = self.find_path_alt(start,end)
+        connections = []
+        for i in lst:
+            connections.append(i.label)
+        return connections
+
+graph_dir = Dir_Graph([a,b,c,d])
