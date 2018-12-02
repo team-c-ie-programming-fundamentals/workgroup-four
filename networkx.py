7e8436c27674c1f70f8d76f1b34fcf610ee0152d
David <dabago@student.ie.edu>
2018-12-02 21:53:15 +0100

    Simplifying code

--- networkx.py
+++ networkx.py
@@ -1,68 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 27 10:55:26 2018
-
-@author: nataliecedeno
-"""
-#%%
-
-G = {
-        "a":["b","c"],
-        "b":["d"],
-        "c":["d"],
-        "d":["e"],
-        "e":[],
-        "f":[]
-        }
-
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.DiGraph()
-G.add_nodes_from(["a","b","c","d","e","f"])
-G.add_edges_from([("a", "b"), ("a", "c"), ("b", "d"), ("c", "d"), ("d","e")])
 
-nx.draw(G,with_labels=True)
-plt.draw()
+# Here we create a directed graph with the functions that are included in 
+#networkx library
+
+
+g = nx.DiGraph()
+
+g.add_edge('a', 'b',weight=5)
+g.add_edge('a', 'c',weight=5)
+g.add_edge('b', 'd',weight=5)
+g.add_edge('c', 'd',weight=10)
+g.add_node('e')
+g.add_edge('d', 'e', weight=1)
+g.add_node('f')
+
+#Network X Functions:
+#%%
+nx.has_path(g, 'a', 'd') #True
+#%%
+nx.has_path(g, 'a', 'e') #True
+#%%
+nx.has_path(g, 'a', 'f') #Fale
+#%%
+nx.dijkstra_path_length(g,"a","e") 
+#%%
+nx.dijkstra_path_length(g,"a","b") 
+#%%
+nx.shortest_path(g, 'a', 'e', weight='weight')
+
+
+#%% 
+#Drawing the graph with different attributes
+nx.draw(g, node_size = 800, node_color="yellow", with_labels = True, font_weight="bold")
 plt.show()
-#%%
-def has_path(G,source,target):
-    try:
-        nx.shortest_path(G, source, target)
-    except nx.NetworkXNoPath:
-        return False
-    return True
-
-#%%
-def shortest_path(G, source, target, weight, method='dijkstra'):
-    if method not in ('dijkstra'):
-        raise ValueError('method not supported: {}'.format(method))
-    method = 'unweighted' if weight is None else method
-    if source is None:
-        if target is None:
-            if method == 'unweighted':
-                paths = dict(nx.all_pairs_shortest_path(G))
-            elif method == 'dijkstra':
-                paths = dict(nx.all_pairs_dijkstra_path(G, weight=weight))
-        else:
-            with nx.utils.reversed(G):
-                if method == 'unweighted':
-                    paths = nx.single_source_shortest_path(G, target)
-                elif method == 'dijkstra':
-                    paths = nx.single_source_dijkstra_path(G, target,weight=weight)
-                for target in paths:
-                    paths[target] = list(reversed(paths[target]))
-    else:
-        if target is None:
-            if method == 'unweighted':
-                paths = nx.single_source_shortest_path(G, source)
-            elif method == 'dijkstra':
-                paths = nx.single_source_dijkstra_path(G, source, weight=weight)
-        else:
-            if method == 'unweighted':
-                paths = nx.bidirectional_shortest_path(G, source, target)
-            elif method == 'dijkstra':
-                paths = nx.dijkstra_path(G, source, target, weight)
-    return paths
-#%%
